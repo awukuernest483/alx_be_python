85cf70500edd974d1fe46481fe61f9b3dc1b981e
Ernest Awuku Junior <awukuernest@gmail.com>
2025-09-09 15:20:52 +0000

    test

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+additionresult = int(number1 + number2)
+subtractionresult = int(number1 - number2)
+multiplicationresult = int(number1 * number2)
+
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(additionresult))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtractionresult))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplicationresult))
+
