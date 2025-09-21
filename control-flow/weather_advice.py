31c5ef9f31c614f6c37a84ace42a96699b34cb21
Ernest Awuku Junior <awukuernest@gmail.com>
2025-09-21 21:42:22 +0000

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,16 +1,16 @@
-weather_conditions=["sunny","rainy","cold"]
-recommendations=["Wear a t-shirt and sunglasses","Don't forget your umbrella and a raincoat.","Make sure to wear a warm coat and a scarf","Sorry, I don't have recommendations for this weather."]
+# weather_conditions=["sunny","rainy","cold"]
+# recommendations=["Wear a t-shirt and sunglasses","Don't forget your umbrella and a raincoat.","Make sure to wear a warm coat and a scarf","Sorry, I don't have recommendations for this weather."]
 selected_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 if (selected_weather == "sunny") : {
-    print (recommendations[0])
+    print ("Wear a t-shirt and sunglasses")
 }
 elif selected_weather == "rainy": {
-    print (recommendations[1])
+    print ("Don't forget your umbrella and a raincoat.")
 } 
 elif selected_weather == "cold" : {
-    print (recommendations[2])
+    print ("Make sure to wear a warm coat and a scarf")
 } 
 else : {
-    print (recommendations[3])
+    print ("Sorry, I don't have recommendations for this weather.")
 }
 
