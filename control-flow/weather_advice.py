6e8fb934b77c64966e31d5bc43292a87571d36d8
Ernest Awuku Junior <awukuernest@gmail.com>
2025-09-21 21:39:56 +0000

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,6 @@
 weather_conditions=["sunny","rainy","cold"]
 recommendations=["Wear a t-shirt and sunglasses","Don't forget your umbrella and a raincoat.","Make sure to wear a warm coat and a scarf","Sorry, I don't have recommendations for this weather."]
-selected_weather = input("What's the weather like today?")
+selected_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 if (selected_weather == weather_conditions[0]) : {
     print (recommendations[0])
 }
