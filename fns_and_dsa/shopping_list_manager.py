e94682e313e9730d56716141c768456f040f33b8
Ernest Awuku Junior <awukuernest@gmail.com>
2025-09-27 23:29:57 +0000

    changes

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,1 +1,30 @@
-shopping_list = []
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            shopping_list.append(input("Enter item to add: "))
+        elif choice == '2':
+            try:
+                shopping_list.remove(input("Enter item to remove: "))
+            except ValueError:
+                print("Item not found in the shopping list.")
+        elif choice == '3':
+            print("Shopping List:", shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
