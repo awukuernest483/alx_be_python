06f5e4e56995bfc7df53ae9b398a24dcceb0dc5a
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-05 21:21:58 +0000

    changes

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,25 +1,38 @@
 class Book:
-    def __init__(self, title, author, _is_checked_out):
+    def __init__(self, title, author, _is_checked_out=False):
       self.title = title
       self.author = author
       self._is_checked_out = _is_checked_out
+      
+    
 
 
 class Library:
     def __init__(self):
         self.books = []
         
-        def add_book():
-            return True
+        
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Added: {book}")
         
         
-        def check_out_book(title):
-            check_out_book(self)
+    def check_out_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                print(f"Checked out: {book.title}")
+                return
+            print(f"Book titled '{title}' not found.")
+        
             
-        def return_book(title):
-            return_book(self)
+    def return_book(self, book):
+        self.books.append(book)
+        return True
+    
             
-        def list_available_books():
-            list_available_books(self)
+    def list_available_books(self):
+        print (self.books)
+        
             
         
