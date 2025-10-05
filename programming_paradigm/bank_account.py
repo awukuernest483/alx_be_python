918fb1e60e0715f2fb67a4df1f336cd1f9d7d5cb
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-05 16:44:57 +0000

    account balance

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, account_balance, initial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"{self.account_balance} is your account balance")
+                  
+                  
