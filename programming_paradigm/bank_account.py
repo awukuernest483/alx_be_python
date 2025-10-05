f60987f77752a392a24dc065017ea6def0827f07
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-05 16:49:17 +0000

    changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,6 +15,6 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print (f"Current Balance: {self.accountbalance}")
+        print (f"Current Balance: ${self.account_balance}")
                   
                   
