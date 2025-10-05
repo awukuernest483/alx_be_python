b39d305dd8c159bddd86034fb3a6cc67399b1bea
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-05 17:50:36 +0000

    changes

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
 def safe_divide(numerator, denominator):
     try:
-        result = numerator / denominator
+        result = float(numerator) / float(denominator)
         return print(f"The result of the division is {result:.1f}")
     except:
         if denominator == 0:
