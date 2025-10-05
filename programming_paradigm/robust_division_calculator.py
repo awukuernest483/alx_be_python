a3e623f337dfa9fd60402a83d01c521c509a11c3
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-05 17:47:45 +0000

    changes

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+        return print(f"The result of the division is {result:.1f}")
+    except:
+        if denominator == 0:
+            return print ("Error: Cannot divide by zero.")
+        else:
+            return print("sklss")
+   
+    
