6d247f778e2225966738fae5ae4e8a7ef5d73836
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-12 13:04:08 +0000

    change

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,13 +5,13 @@ class Book:
         self.year = int(year)
       
     def __str__(self):
-        return f"{self.title}, {self.author}, {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"{self.title}, {self.author}, {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __del__(self):
-        return
+        return 
         
     
   
