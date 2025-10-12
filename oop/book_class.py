38a1ce3da0270281fe7a1d3424d8e486e14eb9c4
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-12 12:59:36 +0000

    changes

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = int(year)
+      
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.year}"
+    
+    def __repr__(self):
+        return f"{self.title}, {self.author}, {self.year}"
+    
+    def __del__(self):
+        return
+        
+    
+  
+    
+
+    
